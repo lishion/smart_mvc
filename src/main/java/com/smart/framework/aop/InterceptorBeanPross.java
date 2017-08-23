4d417c794af67709b0fc8ebe413f0d2c476f280d
lishion <544670411@qq.com>
2017-08-23 18:30:34 +0800

    add aop callback class

---
+++ src/main/java/com/smart/framework/aop/InterceptorBeanPross.java
@@ -0,0 +1,14 @@
+package com.smart.framework.aop;
+
+import com.smart.framework.bean.BeanProcessPreCallback;
+import com.smart.framework.bean.BeanWrapper;
+
+public class InterceptorBeanPross implements BeanProcessPreCallback {
+
+    
+
+    @Override
+    public BeanWrapper process(BeanWrapper beanWrapper) {
+        return null;
+    }
+}
